c4fd9c57e15929b877063cec0a6c0b2e6e69f6fb
PowerpuffBlossom <junalynburgos6@example.com>
2026-09-24 18:41:04 +0800

    Fix Google OAuth production callback URL

--- server/config/passport.js
+++ server/config/passport.js
@@ -14,7 +14,10 @@ module.exports = function(passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback",
+        callbackURL:
+  process.env.NODE_ENV === "production"
+    ? "https://yearbook-server-pgtt.onrender.com/auth/google/callback"
+    : "http://localhost:5000/auth/google/callback",
       },
 
       async (accessToken, refreshToken, profile, done) => {
